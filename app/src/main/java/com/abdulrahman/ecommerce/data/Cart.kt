5237fc6a8d6c26e304b4872cc09b4e4c7d4c7a3e
abdulrahman988 <abdulrahmanmahmoud707@gmail.com>
2023-07-15 00:18:15 +0300

    ShoppingApp

---
+++ app/src/main/java/com/abdulrahman/ecommerce/data/Cart.kt
@@ -0,0 +1,4 @@
+package com.abdulrahman.ecommerce.data
+
+class Cart {
+}
--- app/src/main/java/com/abdulrahman/ecommerce/fragments/product/ProductDetailsFragment.kt
+++ app/src/main/java/com/abdulrahman/ecommerce/fragments/product/ProductDetailsFragment.kt
@@ -1,10 +1,13 @@
 package com.abdulrahman.ecommerce.fragments.product
 
+import android.graphics.Paint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.abdulrahman.ecommerce.databinding.FragmentProductBinding
 import com.bumptech.glide.Glide
@@ -15,8 +18,7 @@ class ProductDetailsFragment : Fragment() {
     private val args: ProductDetailsFragmentArgs by navArgs()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         binding = FragmentProductBinding.inflate(inflater)
         return binding.root
@@ -28,20 +30,34 @@ class ProductDetailsFragment : Fragment() {
         val product = args.product
 
         binding.apply {
-
-            Glide.with(requireContext()).load(product.images[0]).into(ivProductImage)
+            try {
+                Glide.with(requireContext()).load(product.images[0]).into(ivProductImage)
+            } catch (e: Exception) {
+                Log.d("vv", e.message.toString())
+            }
             tvProductName.text = product.name
-            productPrice.text = "$ ${String.format("%.2f",(product.price))}"
-//            productDiscountedPrice.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
-            val discounted = (product.offerPercentage?.times(product.price))!! /100
+            productPrice.text = "$ ${String.format("%.2f", (product.price))}"
 
-            productDiscountedPrice.text = "$ ${String.format("%.2f",(product.price - discounted))}"
-            productOfferPercentage.text = product.offerPercentage.toString()
+            if (product.offerPercentage != 0.toFloat()) {
+                productPrice.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+                val discounted = (product.offerPercentage?.times(product.price))!! / 100
+                productDiscountedPrice.text =
+                    "$ ${String.format("%.2f", (product.price - discounted))}"
+                productOfferPercentage.text = "-${product.offerPercentage.toInt()}%"
+            } else {
+                productOfferPercentage.visibility = View.GONE
+                productDiscountedPrice.visibility = View.GONE
+            }
             productDescription.text = product.description
-            productSize.text = product.sizes!![0]
-            productColor.text = product.colors!![0]
+            productSize.text = "Sizes : ${product.sizes!![0]}"
+            productColor.text = "Colors : ${product.colors!![0]}"
 
+            ivClose.setOnClickListener {
+                findNavController().navigateUp()
+            }
         }
+
+
     }
 }
 
