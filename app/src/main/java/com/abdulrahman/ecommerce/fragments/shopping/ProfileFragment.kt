27c2f91a01c0f346fffeef3a91cf3308d8e95435
abdulrahman988 <abdulrahmanmahmoud707@gmail.com>
2023-07-21 05:36:02 +0300

    ShoppingApp

--- app/src/main/java/com/abdulrahman/ecommerce/fragments/shopping/ProfileFragment.kt
+++ app/src/main/java/com/abdulrahman/ecommerce/fragments/shopping/ProfileFragment.kt
@@ -1,15 +1,21 @@
 package com.abdulrahman.ecommerce.fragments.shopping
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.abdulrahman.ecommerce.activities.LoginRegisterActivity
 import com.abdulrahman.ecommerce.databinding.FragmentProfileBinding
+import com.abdulrahman.ecommerce.viewmodel.ProfileViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ProfileFragment : Fragment() {
     private lateinit var binding: FragmentProfileBinding
-
+    private val viewModel by viewModels<ProfileViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -22,6 +28,10 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.btnLogout.setOnClickListener {
+            viewModel.logout()
+            startActivity(Intent(requireContext(), LoginRegisterActivity::class.java))
 
+        }
     }
 }
---
+++ app/src/main/java/com/abdulrahman/ecommerce/viewmodel/ProfileViewModel.kt
@@ -0,0 +1,68 @@
+package com.abdulrahman.ecommerce.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.abdulrahman.ecommerce.data.User
+import com.abdulrahman.ecommerce.util.Resource
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor(
+    private val auth: FirebaseAuth,
+    private val db: FirebaseFirestore
+) : ViewModel() {
+
+
+    private val _profileImg = MutableStateFlow<Resource<String>>(Resource.Unspecified())
+    val profileImg: Flow<Resource<String>> = _profileImg
+
+    private val _firstName = MutableStateFlow<Resource<String>>(Resource.Unspecified())
+    val firstName: Flow<Resource<String>> = _firstName
+
+    private val _lastName = MutableStateFlow<Resource<String>>(Resource.Unspecified())
+    val lastName: Flow<Resource<String>> = _lastName
+
+    private val _mail = MutableStateFlow<Resource<String>>(Resource.Unspecified())
+    val mail: Flow<Resource<String>> = _mail
+
+
+
+    fun logout() {
+        auth.signOut()
+    }
+
+    fun getProfileInfo() {
+        db.collection("user").document(auth.uid!!).get()
+            .addOnSuccessListener {document ->
+                val imagePath = document.get("imagePath").toString()
+                val firstName = document.get("firstName").toString()
+                val lastName = document.get("lastName").toString()
+                val email = document.get("email").toString()
+
+                viewModelScope.launch {
+                    _profileImg.emit(Resource.Success(imagePath))
+                    _firstName.emit(Resource.Success(firstName))
+                    _lastName.emit(Resource.Success(lastName))
+                    _mail.emit(Resource.Success(email))
+                }
+            }.addOnFailureListener {e ->
+                viewModelScope.launch {
+                    _profileImg.emit(Resource.Error(e.message))
+                    _firstName.emit(Resource.Error(e.message))
+                    _lastName.emit(Resource.Error(e.message))
+                    _mail.emit(Resource.Error(e.message))
+                }
+            }
+    }
+}
+
+
+
+
+
